6e13125190e11fae26901d788e1045e3c6f9693f
Daisuke Nakahara <daisuken@msn.com>
2021-01-22 07:20:50 -0800

    Prototype DPS Function

---
+++ IoT-Plug-and-Play-Workshop-Functions/Dps_Processor.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Provisioning.Service;
+
+namespace IoT_Plug_and_Play_Workshop_Functions
+{
+    public static class Dps_Processor
+    {
+        [FunctionName("Dps_Processor")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            log.LogDebug($"Request.Body: {requestBody}");
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            ResponseObj obj = new ResponseObj();
+
+            return (ActionResult)new OkObjectResult(obj);
+        }
+    }
+
+    public class ResponseObj
+    {
+        public string iotHubHostName { get; set; }
+        public TwinState initialTwin { get; set; }
+    }
+}
