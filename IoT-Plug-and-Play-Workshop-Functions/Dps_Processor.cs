e1f34290607e5ff0423f2b1e5da35321cd2d4b84
Daisuke Nakahara <daisuken@msn.com>
2021-07-27 12:32:34 -0700

    Update Dps_Processor.cs

--- IoT-Plug-and-Play-Workshop-Functions/Dps_Processor.cs
+++ IoT-Plug-and-Play-Workshop-Functions/Dps_Processor.cs
@@ -480,9 +480,9 @@ namespace IoT_Plug_and_Play_Workshop_Functions
                     foreach (var component in components)
                     {
                         DTComponentInfo dtComp = component.Value as DTComponentInfo;
-                        //modelList.Add(dtComp.Schema.Id.AbsoluteUri.ToString());
-                        _logger.LogInformation($"Create Mode {dtComp.Schema.Id.AbsoluteUri.ToString()}");
-                        await CreateTwinModel(dtClient, dtComp.Schema.Id.AbsoluteUri.ToString());
+                        modelList.Add(dtComp.Schema.Id.AbsoluteUri.ToString());
+                        _logger.LogInformation($"Creating Model {dtComp.Schema.Id.AbsoluteUri.ToString()}");
+                        //await CreateTwinModel(dtClient, dtComp.Schema.Id.AbsoluteUri.ToString());
                         //await _adtClient.CreateModelsAsync(modelList);
                         //modelList.Clear();
                     }
